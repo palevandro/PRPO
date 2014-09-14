7c24d7ac12a8f3920217f3f44892351f5b8d38cb
palevandro <palevandro171@gmail.com>
2014-09-15 00:59:42 +0400

    Git

---
+++ Program/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBoxBoldAndItalic = new System.Windows.Forms.CheckBox();
+            this.checkBoxUnderline = new System.Windows.Forms.CheckBox();
+            this.checkBoxItalic = new System.Windows.Forms.CheckBox();
+            this.checkBoxBold = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonBlue = new System.Windows.Forms.RadioButton();
+            this.radioButtonGreen = new System.Windows.Forms.RadioButton();
+            this.radioButtonRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonAuto = new System.Windows.Forms.RadioButton();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.enterTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Controls.Add(this.resultTextBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.resetButton);
+            this.panel1.Controls.Add(this.enterTextBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(840, 611);
+            this.panel1.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(554, 463);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(82, 37);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(160, 478);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(357, 22);
+            this.resultTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(21, 478);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBoxBoldAndItalic);
+            this.groupBox2.Controls.Add(this.checkBoxUnderline);
+            this.groupBox2.Controls.Add(this.checkBoxItalic);
+            this.groupBox2.Controls.Add(this.checkBoxBold);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(24, 324);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(614, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TYPE";
+            // 
+            // checkBoxBoldAndItalic
+            // 
+            this.checkBoxBoldAndItalic.AutoSize = true;
+            this.checkBoxBoldAndItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxBoldAndItalic.Location = new System.Drawing.Point(442, 52);
+            this.checkBoxBoldAndItalic.Name = "checkBoxBoldAndItalic";
+            this.checkBoxBoldAndItalic.Size = new System.Drawing.Size(170, 29);
+            this.checkBoxBoldAndItalic.TabIndex = 3;
+            this.checkBoxBoldAndItalic.Text = "Bold and Italic";
+            this.checkBoxBoldAndItalic.UseVisualStyleBackColor = true;
+            this.checkBoxBoldAndItalic.CheckedChanged += new System.EventHandler(this.checkBoxBoldAndItalic_CheckedChanged);
+            // 
+            // checkBoxUnderline
+            // 
+            this.checkBoxUnderline.AutoSize = true;
+            this.checkBoxUnderline.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxUnderline.Location = new System.Drawing.Point(296, 52);
+            this.checkBoxUnderline.Name = "checkBoxUnderline";
+            this.checkBoxUnderline.Size = new System.Drawing.Size(117, 29);
+            this.checkBoxUnderline.TabIndex = 2;
+            this.checkBoxUnderline.Text = "Underline";
+            this.checkBoxUnderline.UseVisualStyleBackColor = true;
+            this.checkBoxUnderline.CheckedChanged += new System.EventHandler(this.checkBoxUnderline_CheckedChanged);
+            // 
+            // checkBoxItalic
+            // 
+            this.checkBoxItalic.AutoSize = true;
+            this.checkBoxItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxItalic.Location = new System.Drawing.Point(169, 52);
+            this.checkBoxItalic.Name = "checkBoxItalic";
+            this.checkBoxItalic.Size = new System.Drawing.Size(73, 29);
+            this.checkBoxItalic.TabIndex = 1;
+            this.checkBoxItalic.Text = "Italic";
+            this.checkBoxItalic.UseVisualStyleBackColor = true;
+            this.checkBoxItalic.CheckedChanged += new System.EventHandler(this.checkBoxItalic_CheckedChanged);
+            // 
+            // checkBoxBold
+            // 
+            this.checkBoxBold.AutoSize = true;
+            this.checkBoxBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxBold.Location = new System.Drawing.Point(19, 52);
+            this.checkBoxBold.Name = "checkBoxBold";
+            this.checkBoxBold.Size = new System.Drawing.Size(77, 29);
+            this.checkBoxBold.TabIndex = 0;
+            this.checkBoxBold.Text = "Bold";
+            this.checkBoxBold.UseVisualStyleBackColor = true;
+            this.checkBoxBold.CheckedChanged += new System.EventHandler(this.checkBoxBold_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonBlue);
+            this.groupBox1.Controls.Add(this.radioButtonGreen);
+            this.groupBox1.Controls.Add(this.radioButtonRed);
+            this.groupBox1.Controls.Add(this.radioButtonAuto);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(24, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(614, 129);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COLOR OF TEXT";
+            // 
+            // radioButtonBlue
+            // 
+            this.radioButtonBlue.AutoSize = true;
+            this.radioButtonBlue.ForeColor = System.Drawing.Color.Blue;
+            this.radioButtonBlue.Location = new System.Drawing.Point(442, 58);
+            this.radioButtonBlue.Name = "radioButtonBlue";
+            this.radioButtonBlue.Size = new System.Drawing.Size(68, 26);
+            this.radioButtonBlue.TabIndex = 5;
+            this.radioButtonBlue.TabStop = true;
+            this.radioButtonBlue.Text = "Blue";
+            this.radioButtonBlue.UseVisualStyleBackColor = true;
+            this.radioButtonBlue.CheckedChanged += new System.EventHandler(this.radioButtonBlue_CheckedChanged);
+            // 
+            // radioButtonGreen
+            // 
+            this.radioButtonGreen.AutoSize = true;
+            this.radioButtonGreen.ForeColor = System.Drawing.Color.Lime;
+            this.radioButtonGreen.Location = new System.Drawing.Point(296, 58);
+            this.radioButtonGreen.Name = "radioButtonGreen";
+            this.radioButtonGreen.Size = new System.Drawing.Size(79, 26);
+            this.radioButtonGreen.TabIndex = 2;
+            this.radioButtonGreen.TabStop = true;
+            this.radioButtonGreen.Text = "Green";
+            this.radioButtonGreen.UseVisualStyleBackColor = true;
+            this.radioButtonGreen.CheckedChanged += new System.EventHandler(this.radioButtonGreen_CheckedChanged);
+            // 
+            // radioButtonRed
+            // 
+            this.radioButtonRed.AutoSize = true;
+            this.radioButtonRed.ForeColor = System.Drawing.Color.Red;
+            this.radioButtonRed.Location = new System.Drawing.Point(169, 58);
+            this.radioButtonRed.Name = "radioButtonRed";
+            this.radioButtonRed.Size = new System.Drawing.Size(63, 26);
+            this.radioButtonRed.TabIndex = 1;
+            this.radioButtonRed.TabStop = true;
+            this.radioButtonRed.Text = "Red";
+            this.radioButtonRed.UseVisualStyleBackColor = true;
+            this.radioButtonRed.CheckedChanged += new System.EventHandler(this.radioButtonRed_CheckedChanged);
+            // 
+            // radioButtonAuto
+            // 
+            this.radioButtonAuto.AutoSize = true;
+            this.radioButtonAuto.Location = new System.Drawing.Point(19, 58);
+            this.radioButtonAuto.Name = "radioButtonAuto";
+            this.radioButtonAuto.Size = new System.Drawing.Size(120, 26);
+            this.radioButtonAuto.TabIndex = 0;
+            this.radioButtonAuto.TabStop = true;
+            this.radioButtonAuto.Text = "Auto Color";
+            this.radioButtonAuto.UseVisualStyleBackColor = true;
+            this.radioButtonAuto.CheckedChanged += new System.EventHandler(this.radioButtonAuto_CheckedChanged);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(560, 95);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(82, 37);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // enterTextBox
+            // 
+            this.enterTextBox.Location = new System.Drawing.Point(160, 105);
+            this.enterTextBox.Name = "enterTextBox";
+            this.enterTextBox.Size = new System.Drawing.Size(357, 22);
+            this.enterTextBox.TabIndex = 2;
+            this.enterTextBox.TextChanged += new System.EventHandler(this.enterTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Text :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(284, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COLOR OF TEXT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 611);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxBoldAndItalic;
+        private System.Windows.Forms.CheckBox checkBoxUnderline;
+        private System.Windows.Forms.CheckBox checkBoxItalic;
+        private System.Windows.Forms.CheckBox checkBoxBold;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonBlue;
+        private System.Windows.Forms.RadioButton radioButtonGreen;
+        private System.Windows.Forms.RadioButton radioButtonRed;
+        private System.Windows.Forms.RadioButton radioButtonAuto;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TextBox enterTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Program/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            // Set original value when click reset button
+            enterTextBox.ResetText();
+            resetButton.ResetText();
+            //reset Radio Button ( Color)
+            resetButton.ForeColor = Color.Black;
+            radioButtonAuto.Checked = false;
+            radioButtonBlue.Checked = false;
+            radioButtonGreen.Checked = false;
+            radioButtonRed.Checked = false;
+            //reset CheckBox Button (Type)
+            checkBoxItalic.Checked = false;
+            checkBoxBold.Checked = false;
+            checkBoxUnderline.Checked = false;
+            checkBoxBoldAndItalic.Checked = false;
+
+        }
+
+        private void enterTextBox_TextChanged(object sender, EventArgs e)
+        {
+            resultTextBox.Text = enterTextBox.Text;
+        }
+
+        private void radioButtonRed_CheckedChanged(object sender, EventArgs e)
+        {
+            resultTextBox.ForeColor = Color.Red;           
+        }
+
+        private void radioButtonGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            resultTextBox.ForeColor = Color.Green;
+        }
+
+        private void radioButtonBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            resultTextBox.ForeColor = Color.Blue;
+        }
+
+        private void radioButtonAuto_CheckedChanged(object sender, EventArgs e)
+        {
+            Random color = new Random();
+            resultTextBox.ForeColor = Color.FromArgb(color.Next(255),  color.Next(255),  color.Next(255) );
+        }
+
+        private void checkBoxBold_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxBold.Checked == true)
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Bold);
+            else
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Regular);
+        }
+
+        private void checkBoxItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxItalic.Checked == true)                
+            {
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Italic);
+            } 
+            else
+            {
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Regular);
+            }
+        }
+
+        private void checkBoxUnderline_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxUnderline.Checked == true)
+            {
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Underline);
+            } 
+            else
+            {
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Regular);
+            }
+        }
+
+        private void checkBoxBoldAndItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxBoldAndItalic.Checked == true)
+            {
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Bold | FontStyle.Italic);
+            } 
+            else
+            {
+                resultTextBox.Font = new Font(resultTextBox.Font, FontStyle.Regular);
+            }
+        }
+
+
+
+    }
+}
